3270bb0000a85d636b05e7fa002885bde9705774
sandroUnipar <54459325+sandroUnipar@users.noreply.github.com>
2020-06-12 23:30:58 -0300

    Update cliente.entidade.ts

---
+++ src/cliente.entidade.ts
@@ -0,0 +1,12 @@
+ export class ClienteEntidade {
+    nome: string;
+    idade: number;
+    email: string;
+    telefone: number;
+    cpf: number;
+    id: number;
+    enderenco: string;
+    rg: number;
+    livro_favorito: string;
+    Estado_civil: string;
+}
